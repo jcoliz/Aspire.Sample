83b4bb7a47711683862cffdbdb1942eae9fc3c9f
James Coliz <jcoliz@outlook.com>
2024-07-11 16:10:02 -0700

    build(ent): Add IDataProvider interface

---
+++ Aspire.Sample.Entities/Providers/IDataProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace Aspire.Sample.Providers;
+
+/// <summary>
+/// Defines a services to provide data into the system
+/// </summary>
+public interface IDataProvider
+{
+    /// <summary>
+    /// Retrieves a querable set of <typeparamref name="TEntity"/> objects
+    /// </summary>
+    /// <typeparam name="TEntity">The type of entity being operated on by this set.</typeparam>
+    /// <returns>querable set of <typeparamref name="TEntity"/> objects</returns>
+    IQueryable<TEntity> Set<TEntity>() where TEntity : class;
+}
