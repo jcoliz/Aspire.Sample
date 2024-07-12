56bd9d177833f2afe8d43b18c0dc61f2a6322937
James Coliz <jcoliz@outlook.com>
2024-07-12 13:34:26 -0700

    feat(data): Add sample data migration
    Feature complete for moving the sample to postgres. Whew!!!

---
+++ Aspire.Sample.Data.Postgres/Migrations/20240712202943_SeedData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Aspire.Sample.Data.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "WeatherForecasts",
+                columns: new[] { "Id", "Date", "Summary", "TemperatureC" },
+                values: new object[,]
+                {
+                    { 1, new DateOnly(2024, 7, 11), "Seeded", 101 },
+                    { 2, new DateOnly(2024, 7, 10), "Seeded", 102 },
+                    { 3, new DateOnly(2024, 7, 9), "Seeded", 103 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "WeatherForecasts",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "WeatherForecasts",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "WeatherForecasts",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
