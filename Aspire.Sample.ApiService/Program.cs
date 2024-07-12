ef0844ffb44ea3298b9b4c070c93ed5c3488e6a1
James Coliz <jcoliz@outlook.com>
2024-07-12 12:41:54 -0700

    feat(api): Add postgres database to

--- Aspire.Sample.ApiService/Program.cs
+++ Aspire.Sample.ApiService/Program.cs
@@ -1,12 +1,19 @@
+using Aspire.Sample.Data;
 using Aspire.Sample.Models;
+using Aspire.Sample.Providers;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add service defaults & Aspire components.
 builder.AddServiceDefaults();
 
+builder.AddNpgsqlDbContext<ApplicationDbContext>("forecasts");
+
 // Add services to the container.
 builder.Services.AddProblemDetails();
+
+builder.Services.AddScoped<IDataProvider, ApplicationDbContext>();
 
 var app = builder.Build();
 
@@ -18,20 +25,29 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast", (IDataProvider data) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            0,
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+        //var query = data.Get<WeatherForecast>().OrderByDescending(x=>x.Date).Take(10);
+
+        //var forecasts = await data.ToListNoTrackingAsync(query);
+
+        return Array.Empty<WeatherForecast>();
+        //return forecasts.ToArray();
+
+    //return [];
 });
 
 app.MapDefaultEndpoints();
+
+//var scope = app.Services.CreateScope();
+
+//var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+// Postgres databases are always brought current by the application,
+// because they are only ever used for development and testing.
+//
+// SqlServer databases must be created by EF Core tooling or SQL scripts!
+
+//db.Database.Migrate();
 
 app.Run();
--- Aspire.Sample.AppHost/Program.cs
+++ Aspire.Sample.AppHost/Program.cs
@@ -2,11 +2,25 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 
-var apiService = builder.AddProject<Projects.Aspire_Sample_ApiService>("apiservice");
+var postgresDb = builder.AddPostgres("postgres")
+    // Set the name of the default database to auto-create on container startup.
+    .WithEnvironment("POSTGRES_DB", "forecasts")
+    // Add the default database to the application model so that it can be referenced by other resources.
+    .AddDatabase("forecasts");
+
+var apiService = builder.AddProject<Projects.Aspire_Sample_ApiService>("apiservice")
+    .WithReference(postgresDb);
 
 builder.AddProject<Projects.Aspire_Sample_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(cache)
-    .WithReference(apiService);
+    .WithReference(apiService)
+    .WithReference(postgresDb);
 
-builder.Build().Run();
+var app = builder.Build();
+
+//var cstr = await postgresDb.Resource.Parent.GetConnectionStringAsync();
+
+//Console.WriteLine("Connection string: {0}", cstr);
+
+app.Run();
