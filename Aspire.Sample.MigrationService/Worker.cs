6023d6b142ee517e6eeb8202a00b0cd6fdf4b817
James Coliz <jcoliz@outlook.com>
2024-07-12 13:23:25 -0700

    feat(migrator): Successfully migrate on launch
    Now shows empty data no problem

--- Aspire.Sample.MigrationService/Worker.cs
+++ Aspire.Sample.MigrationService/Worker.cs
@@ -1,25 +1,47 @@
 using Aspire.Sample.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Aspire.Sample.MigrationService;
 
-public partial class Worker(ILogger<Worker> logger, IServiceProvider serviceProvider) : BackgroundService
+public partial class Worker(ILogger<Worker> logger, IServiceProvider serviceProvider, IHostApplicationLifetime lifetime) : BackgroundService
 {
     public readonly ILogger _logger = logger;
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
+        try
         {
-            using (var scope = serviceProvider.CreateScope())
-            {
-                var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                var ok = dbContext.Database.CanConnect();
-                LogHeartbeat(ok);
-            }
-            await Task.Delay(1000, stoppingToken);
+            using var scope = serviceProvider.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            await RunMigrationAsync(dbContext, stoppingToken);
+
+            LogOk();
         }
+        catch (Exception ex)
+        {
+            LogFail(ex);
+        }
+
+        lifetime.StopApplication();
     }
 
-    [LoggerMessage(Level = LogLevel.Information, Message = "Database status: {CanConnect}")]
-    public partial void LogHeartbeat(bool canConnect);
+
+    private static async Task RunMigrationAsync(DbContext dbContext, CancellationToken cancellationToken)
+    {
+        var strategy = dbContext.Database.CreateExecutionStrategy();
+        await strategy.ExecuteAsync(async () =>
+        {
+            // Run migration in a transaction to avoid partial migration if it fails.
+            await using var transaction = await dbContext.Database.BeginTransactionAsync(cancellationToken);
+            await dbContext.Database.MigrateAsync(cancellationToken);
+            await transaction.CommitAsync(cancellationToken);
+        });
+    }
+
+    [LoggerMessage(Level = LogLevel.Information, Message = "Database migration: OK")]
+    public partial void LogOk();
+
+    [LoggerMessage(Level = LogLevel.Critical, Message = "Database migration: Failed")]
+    public partial void LogFail(Exception ex);
 }
