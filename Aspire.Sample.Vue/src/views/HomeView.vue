56a37cd9dccd27e6455a489446406aade55a9afd
James Coliz <jcoliz@outlook.com>
2024-07-17 14:52:40 -0700

    build(vue): Clean up styles to match blazor

--- Aspire.Sample.Vue/src/views/HomeView.vue
+++ Aspire.Sample.Vue/src/views/HomeView.vue
@@ -3,7 +3,5 @@ import HelloWorld from '@/components/HelloWorld.vue';
 </script>
 
 <template>
-  <main data-test-id="HomeView">
     <HelloWorld msg="Hello, Aspire.Sample!" />
-  </main>
 </template>
