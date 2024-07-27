2992b50f17f9c9656705373e7f0def29675d3e1e
James Coliz <jcoliz@outlook.com>
2024-07-26 18:27:31 -0700

    feat(sqlite): Add initial migration

---
+++ Aspire.Sample.Data.Sqlite/Migrations/20240727012627_Initial.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Aspire.Sample.Data.Sqlite.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WeatherForecasts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Date = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    TemperatureC = table.Column<int>(type: "INTEGER", nullable: false),
+                    Summary = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeatherForecasts", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "WeatherForecasts",
+                columns: new[] { "Id", "Date", "Summary", "TemperatureC" },
+                values: new object[,]
+                {
+                    { 1, new DateOnly(2024, 7, 25), "Seeded", 101 },
+                    { 2, new DateOnly(2024, 7, 24), "Seeded", 102 },
+                    { 3, new DateOnly(2024, 7, 23), "Seeded", 103 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WeatherForecasts");
+        }
+    }
+}
