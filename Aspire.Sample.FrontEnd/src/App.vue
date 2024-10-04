b3b1c73844d012544b55a4a27339441b20ed0943
James Coliz <jcoliz@outlook.com>
2024-10-04 09:50:14 -0700

    refactor(frontend): Remove unused files

--- Aspire.Sample.FrontEnd/src/App.vue
+++ Aspire.Sample.FrontEnd/src/App.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import AppLayout from './layout/AppLayout.vue';
 </script>
 
