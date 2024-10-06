beccfc644b8442aaf6ae1b16e5cc4ffec0fa837f
James Coliz <jcoliz@outlook.com>
2024-10-06 14:52:19 -0700

    feat(nuxt): Add index page and HelloWorld component

---
+++ Aspire.Sample.FrontEnd.Nuxt/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+defineProps<{
+  msg: string
+}>()
+
+</script>
+
+<template>
+  <div class="greetings" data-test-id="HelloWorld">
+    <PrimeToast/>
+    <h1 class="green">{{ msg }}</h1>
+    <p>
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+    </p>
+  </div>
+</template>
+
+<style scoped>
+</style>
