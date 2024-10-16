bd6923c476d62252e95b7af75d2a2436f8b121e9
James Coliz <jcoliz@outlook.com>
2024-10-16 13:39:55 -0700

    feat(sqlite): Add migration WeatherGovSchema

--- Aspire.Sample.Data.Sqlite/Migrations/20241016203742_WeatherGovSchema.Designer.cs
+++ Aspire.Sample.Data.Sqlite/Migrations/20241016203742_WeatherGovSchema.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Aspire.Sample.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Aspire.Sample.Data.Sqlite.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241016203742_WeatherGovSchema")]
+    partial class WeatherGovSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.10");
---
+++ Aspire.Sample.Data.Sqlite/Migrations/20241016203742_WeatherGovSchema.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Aspire.Sample.Data.Sqlite.Migrations
+{
+    /// <inheritdoc />
+    public partial class WeatherGovSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "WeatherForecasts",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "WeatherForecasts",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "WeatherForecasts",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.RenameColumn(
+                name: "TemperatureC",
+                table: "WeatherForecasts",
+                newName: "TemperatureF");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TemperatureF",
+                table: "WeatherForecasts",
+                newName: "TemperatureC");
+
+            migrationBuilder.InsertData(
+                table: "WeatherForecasts",
+                columns: new[] { "Id", "Date", "Summary", "TemperatureC" },
+                values: new object[,]
+                {
+                    { 1, new DateOnly(2024, 7, 25), "Seeded", 101 },
+                    { 2, new DateOnly(2024, 7, 24), "Seeded", 102 },
+                    { 3, new DateOnly(2024, 7, 23), "Seeded", 103 }
+                });
+        }
+    }
+}
